af8bb67143b5f62e736a5353d560a9811c020348
alexforsale <alexforsale@yahoo.com>
2023-11-04 20:34:17 +0700

    fish: add completion

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,14 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     [ -d "$HOME/.local/bin" ] && set -x PATH "$HOME/.local/bin" $PATH
+
+    # completions
+    for dir in /usr/share/fish/completions \
+        /usr/local/share/fish/completions \
+        ~/.config/fish/completions
+
+        if test -d $dir
+            source $dir/*
+        end
+    end
 end
